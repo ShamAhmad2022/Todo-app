de7eb7dab0740e55335f3a5b85e76eff81ef5016
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-09-12 13:39:28 +0000

    edits for building

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import AppHeader from "./Components/Header";
 import SettingsPage from "./Components/settingsPage/SettingsPage";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Login from "./Components/Login ";
 import LoginProvider from "./Conext/AuthContext";
 
 export default class App extends React.Component {
